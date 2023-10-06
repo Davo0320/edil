e971ceba7485b07b967f64ac8d8076769c905899
Davo0320 <barrerada320@gmail.com>
2023-10-06 17:06:01 -0500

    read

--- Js/index.js
+++ Js/index.js
@@ -6,6 +6,7 @@ function app() {
     crud.create({ message: "hola mundo" });
     crud.update(1, "hola mundo2");
     crud.delete(1);
+    crud.read(3);
 
     let crud2 = new CRUD("ejemplo");
     console.log(crud2.readAll());
